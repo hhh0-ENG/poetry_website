285a556636cf3e400bf2f3cf0ee5fbce5ff010a3
hhh0-ENG <2485586985@qq.com>
2026-04-06 16:19:42 +0800

    Create App.vue

---
+++ poetry-website/frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Header />
+    <main class="main">
+      <router-view />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+</script>
+
+<style lang="scss">
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.main {
+  flex: 1;
+  padding: 30px 0;
+}
+</style>
