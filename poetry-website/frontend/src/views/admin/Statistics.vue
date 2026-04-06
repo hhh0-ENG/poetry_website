6a5a45b0975d2521aa2be1111a1d69a08f36d32a
hhh0-ENG <2485586985@qq.com>
2026-04-06 16:20:59 +0800

    Create Statistics.vue

---
+++ poetry-website/frontend/src/views/admin/Statistics.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="admin-page">
+    <div class="page-header">
+      <h1>数据统计</h1>
+    </div>
+    <el-empty description="功能开发中..." />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.admin-page {
+  .page-header {
+    margin-bottom: 20px;
+
+    h1 {
+      font-size: 24px;
+      color: #333;
+    }
+  }
+}
+</style>
