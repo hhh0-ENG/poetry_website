bef3c129db461a1b8d0e9d340d28aca9bbabb949
hhh0-ENG <2485586985@qq.com>
2026-04-06 16:19:47 +0800

    Create Footer.vue

---
+++ poetry-website/frontend/src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <p>&copy; 2024 古诗词文化网 All Rights Reserved.</p>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+  background: #333;
+  color: #fff;
+  padding: 30px 0;
+  text-align: center;
+
+  p {
+    color: #999;
+    font-size: 14px;
+    margin: 0;
+  }
+}
+</style>
